fde57a8d03dc25569bcd3a0e23405a7b356d8f59
John Rykken <john.rykken@gmail.com>
2020-03-22 08:05:20 -0700

    Add functionality to iterate backwards given negative step arg

--- chapter-four.js
+++ chapter-four.js
@@ -1,9 +1,12 @@
 const range = (start, end, step) => {
   let arr = []
-  let counter = start
-  while ( counter <= end ) {
+  const negative = Math.sign(step) === -1 ? true : false
+  let counter = negative ? end : start
+  let iterate = true 
+  while (iterate) {
     arr.push(counter)
-    counter = step ? counter + step : counter + 1 
+    counter = step ? counter + step : counter + 1
+    iterate = negative ? counter >= start : counter <= end 
   } 
   return arr
 }
@@ -11,14 +14,12 @@ console.log('range 1, 10', range(1, 10))
 console.log('range 55, 99', range(55, 99))
 console.log('range 1, 10, step 2', range(1, 10, 2))
 console.log('range 847, 1046, step 8', range(847, 1046, 8))
+console.log('range 1, 10, -2', range(1, 10, -2))
 
 const sum = arr => {
-  let sum = 0
-  for (let a of arr ) {
-    if (a === arr[0]) sum = a 
-    else sum = sum + a
-  }
+  let sum
+  for (let a of arr) sum = a === arr[0] ? a : sum + a   
   return sum 
 }
-// console.log('sum of range 1, 10', sum(range(1,10)))
-// console.log('sum of range sum', (range(55,99)))
+console.log('sum of range 1, 10', sum(range(1,10)))
+console.log('sum of range sum', (range(55,99)))
