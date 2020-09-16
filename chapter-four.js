8b566e04fd11d31e49819dbec2f28c1560b62133
John Rykken <john.rykken@gmail.com>
2020-09-16 14:14:50 -0700

    add comments showing expected output

--- chapter-four.js
+++ chapter-four.js
@@ -10,11 +10,11 @@ const range = (start, end, step) => {
   } 
   return arr
 }
-console.log('range 1, 10', range(1, 10))
-console.log('range 55, 99', range(55, 99))
-console.log('range 1, 10, step 2', range(1, 10, 2))
-console.log('range 847, 1046, step 8', range(847, 1046, 8))
-console.log('range 1, 10, -2', range(1, 10, -2))
+console.log('range 1, 10', range(1, 10)) // -> [1,2,3,4,5,6,7,8,9,10]
+console.log('range 55, 99', range(55, 99)) // -> [55,56,57,58,59,60,61...99]
+console.log('range 1, 10, step 2', range(1, 10, 2)) // -> [1,3,5,7,9]
+console.log('range 847, 1046, step 8', range(847, 1046, 8)) // -> [847,855,863,871,879,887,895,903,911,919...1039]
+console.log('range 1, 10, -2', range(1, 10, -2)) // -> [10,8,6,4,2]
 
 console.log('************************************')
 console.log('************************************')
@@ -24,8 +24,8 @@ const sum = arr => {
   for (let a of arr) sum = a === arr[0] ? a : sum + a   
   return sum 
 }
-console.log('sum of range 1, 10', sum(range(1,10)))
-console.log('sum of range 55, 59', sum(range(55,99)))
+console.log('sum of range 1, 10', sum(range(1,10))) // -> 55
+console.log('sum of range 55, 59', sum(range(55,99))) // -> 3465
 
 console.log('************************************')
 console.log('************************************')
@@ -35,8 +35,8 @@ const reverseArray = arr => {
   for (let i = arr.length - 1; i >= 0; i -= 1) a.push(arr[i])
   return a
 }
-console.log('range 10, 100, step 2', range(10, 100, 2))
-console.log('reverse array', reverseArray(range(10, 100, 2)))
+console.log('reverse array', reverseArray(range(10, 100, 2))) // -> [100,98,96,94...10]
+console.log('reverse arr', reverseArray(['foo', 'bar', 'baz'])) // -> ['baz', 'bar', 'foo']
 
 console.log('************************************')
 console.log('************************************')
@@ -51,7 +51,7 @@ const arrayToList = arr => {
   }
   return list 
 }
-console.log('array to list', arrayToList(listArray))
+console.log('array to list', arrayToList(listArray)) // -> { value: 1, rest: { value: 2, rest: { value: 3, rest: null } } }
 
 console.log('************************************')
 console.log('************************************')
@@ -64,7 +64,7 @@ const listToArray = (obj, arr = []) => {
   else listToArray(obj, arr)
   return arr
 }
-console.log('list to array', listToArray(arrayList))
+console.log('list to array', listToArray(arrayList)) // -> [1,2,3]
 
 console.log('************************************')
 console.log('************************************')
@@ -75,7 +75,7 @@ const listToArrayWithPrepend = (list, element) => {
   else list = { a: element, value: list.value, rest: null }   
   return list 
 }
-console.log('list to array with prepend', listToArrayWithPrepend(arrayList, element))
+console.log('list to array with prepend', listToArrayWithPrepend(arrayList, element)) // -> { a: 'x', value: 1, rest: { a: 'x', value: 2, rest: { a: 'x', value: 3, rest: null } } }
 
 console.log('************************************')
 console.log('************************************')
@@ -98,7 +98,7 @@ const nth = (list, num, count = 1) => {
   else console.log('something went wrong in the nth function')
   return result
 }
-console.log('nth', nth(nthList, nthNum))
+console.log('nth', nth(nthList, nthNum)) // -> 16
 
 console.log('************************************')
 console.log('************************************')
